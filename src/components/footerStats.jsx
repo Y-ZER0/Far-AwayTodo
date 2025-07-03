404c397043dc65dfe8bd06650bece94b75b7fcd4
Yousef Abu-nimreh <150347338+Y-ZER0@users.noreply.github.com>
2025-07-03 11:21:52 +0300

    changed format style in footer stats

--- src/components/footerStats.jsx
+++ src/components/footerStats.jsx
@@ -3,8 +3,15 @@ import "../styling/footerStats.css";
 function FooterStats({ items, checkedItems }) {
   return (
     <footer className="footer-stats">
-      You have {items.length} items in your list , and you already packed{" "}
-      {checkedItems} items
+      {items.length > 0 ? (
+        <>
+          You have {items.length} items in your list, and you already packed{" "}
+          {checkedItems} / {items.length} (
+          {((checkedItems / items.length) * 100).toFixed(2)}%)
+        </>
+      ) : (
+        <>Your list is empty.</>
+      )}
     </footer>
   );
 }
